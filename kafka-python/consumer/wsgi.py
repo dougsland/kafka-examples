72932f522e45d988a338adccd67e66399f50b308
Douglas Schilling Landgraf <dougsland@gmail.com>
2019-03-11 18:17:51 -0400

    testing

--- kafka-python/consumer/wsgi.py
+++ kafka-python/consumer/wsgi.py
@@ -13,9 +13,9 @@ logging.info("Initiating Kafka connection for Consumer...")
 #consumer = KafkaConsumer(os.environ.get('CONSUME_TOPIC'),
 #						 bootstrap_servers=os.environ.get('BOOTSTRAP_SERVERS'))
 logging.info("Kafka consumer:")
-logging.info(consumer)
+#logging.info(consumer)
 
 # Consume things
-for msg in consumer:
-	logging.info("Receiving message:")
-	logging.info(msg)
+#for msg in consumer:
+#	logging.info("Receiving message:")
+#	logging.info(msg)
