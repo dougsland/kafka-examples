10af2e14b7ed79319080f266031a138858ae35f1
Douglas Schilling Landgraf <dougsland@gmail.com>
2019-05-01 15:40:04 -0400

    wsgi: testing

--- kafka-python/consumer/wsgi.py
+++ kafka-python/consumer/wsgi.py
@@ -184,6 +184,8 @@ if __name__ == '__main__':
             }
             logging.info("payload: {0}".format(output))
             output = json.dumps(output).encode()
+
+            logging("JSON {0}".format(output))
             producer.send(os.environ.get('PRODUCE_TOPIC'), output)
 
         logging.info("Removing {0}".format(JSON_TAR_GZ))
