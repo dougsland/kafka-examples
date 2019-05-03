623169258f7ffe831d326bcebbdc07e039364326
Douglas Schilling Landgraf <dougsland@gmail.com>
2019-05-03 03:09:01 -0400

    wsgi: testing

--- kafka-python/consumer/wsgi.py
+++ kafka-python/consumer/wsgi.py
@@ -57,20 +57,20 @@ def parse_json(jsonfile, host_id):
             details.update({
                 'description': 'The following CPUs are deprecated in 4.3. If you have a cluster with one of these CPU families, you must change to another CPU family prior to upgrading the cluster',
                 'kb': '',
-                'result': '''[
+                'result': "[
                 [
                     {
-                        "CPU": "Intel Conroe Family",
-                        "Cluster": "Default"
+                        'CPU': 'Intel Conroe Family',
+                        'Cluster': 'Default'
                     }
                 ],
                 [
                     {
-                        "CPU": "Intel Conroe Family",
-                        "Cluster": "test"
+                        'CPU': 'Intel Conroe Family',
+                        'Cluster': 'SuperCluster'
                     }
                 ]
-            ]'''
+            ]"
             })
             hits.append({'rule_id': "check_deprecated_CPUs_in_4_3|CHECK_DEPRECATED_CPUS_IN_4_3", 'details': details})
 
